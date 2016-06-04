943bd5ef2d9ca18b2e228ae418425b00b3149dc8
Gregory Dymarek <gregd72002@gmail.com>
2016-06-04 18:57:08 +0000

    Adding motors chart

--- www/p_charts/content.php
+++ www/p_charts/content.php
@@ -5,7 +5,23 @@
 	CHARTS
 </p>
 		<p class="llabel">Update rate: <span class="value" id="rate"/></p>
+
+		<div id="charttype">
+			<div class="radio radio-inline" id="optaccelw">
+			  <label><input type="radio" name="optradio" id="optaccel">ACCEL</label>
+			</div>
+			<div class="radio radio-inline" id="optgyrow">
+			  <label><input type="radio" name="optradio" id="optgyro">GYRO</label>
+			</div>
+			<div class="radio radio-inline" id="optmagw">
+			  <label><input type="radio" name="optradio" id="optmag">MAG</label>
+			</div>
+			<div class="radio radio-inline" id="optmotorw">
+			  <label><input type="radio" name="optradio" id="optmotor">MOTOR</label>
+			</div>
+		</div>
 		<hr>
+				
 		<div style="display: hidden;" id="accel">
 			<div style="height: 400px;" id="chart_a"></div>
 			<p class="llabel">accx: <span class="value" id="accx"/></p>
@@ -26,7 +42,19 @@
 			<p class="llabel">magy: <span class="value" id="magy"/></p>
 			<p class="llabel">magz: <span class="value" id="magz"/></p>
 		</div>
-	
+
+		<div style="display: hidden;" id="motor">
+			<div style="height: 600px;" id="chart_motor"></div>		
+			<p class="llabel">motor1: <span class="value" id="motor1"/></p>
+			<p class="llabel">motor2: <span class="value" id="motor2"/></p>
+			<p class="llabel">motor3: <span class="value" id="motor3"/></p>
+			<p class="llabel">motor4: <span class="value" id="motor4"/></p>
+			<p class="llabel">motor5: <span class="value" id="motor5"/></p>
+			<p class="llabel">motor6: <span class="value" id="motor6"/></p>
+			<p class="llabel">motor7: <span class="value" id="motor7"/></p>
+			<p class="llabel">motor8: <span class="value" id="motor8"/></p>					
+		</div>
+
 <hr/>
 </div>
 
@@ -47,11 +75,16 @@ function on_ready() {
 
     sensor = null;
 
+    msgid = 0;
+
+    charttype = null;
+
 	dataLength = 500;
 	xVal = 0;
 	dps_g = [[],[],[]];
 	dps_m = [[],[],[]]; 
-	dps_a = [[],[],[]];    
+	dps_a = [[],[],[]]; 
+	dps_motor = [[],[],[],[]];    
 }
 
 
@@ -60,7 +93,7 @@ function start() {
 	var msg;
 	initialized = 0;
 
-	msg = mw.filters([101,102]); //filters need to be sent as the first message on a new connection to mw proxy
+	msg = mw.filters([101,102, 104]); //filters need to be sent as the first message on a new connection to mw proxy
 	ws.send( msg );
 
 	function req_status() {
@@ -80,17 +113,16 @@ function init_with_status(data) {
 
 	sensor = data.sensor;
 
-	$("#gyro").show();
-	  //we always have gyroscope
-	chart_gyro();
+	//we always have gyroscope
+	//$("#optgyrow").removeClass("disabled");
+	//$("#optmotor").removeClass("disabled");
+	
 
 	if (data.sensor.acc) {
-		$("#accel").show();
-		chart_accel();
+		//$("#optaccelw").removeClass("disabled");
 	}
 	if (data.sensor.mag) {
-		$("#mag").show();
-		chart_mag();
+		//$("#optmagw").removeClass("disabled");
 	}
 
 	setInterval(update,50); //keep sending the requests every 200ms
@@ -103,26 +135,52 @@ function update_rate() {
 }
 
 function update() {
+	var _charttype = $('#charttype input:radio:checked')[0];
+	if (!_charttype) return;
+	if (charttype != _charttype.id) {
+		charttype = _charttype.id;
+		$("#accel").hide();
+		$("#gyro").hide();
+		$("#mag").hide();
+		$("#motor").hide();
+
+		msgid=102;
+		if (charttype=="optaccel") {
+			$("#accel").show();
+			chart_accel();
+		} else if (charttype=="optgyro") {
+			$("#gyro").show();
+			chart_gyro();
+		} else if (charttype=="optmag") {
+			$("#mag").show();
+			chart_mag();
+		} else if (charttype=="optmotor") {
+			$("#motor").show();
+			chart_motor();
+			msgid=104;
+		}
+	}
+		
+	if (msgid==0) return;
 
 	var msg;
 
 	msg = mw.serialize({
-		"id": 102
+		"id": msgid
 	});
 	
 	ws.send(msg);
 
-	chart_g.render();
-	if (sensor.acc) chart_a.render();
-	if (sensor.mag) chart_m.render();
+	if (chart_g.render) chart_g.render();
+	if (chart_a.render) chart_a.render();
+	if (chart_m.render) chart_m.render();
+	if (chart_motor.render) chart_motor.render();
 }
 
 function chart_add(data) {
-	//if (xVal==0) accz0 = data.accz;
-	accz0 = 0;
 	dps_a[0].push({'x':xVal, 'y':data.accx});
 	dps_a[1].push({'x':xVal, 'y':data.accy});
-	dps_a[2].push({'x':xVal, 'y':data.accz-accz0});
+	dps_a[2].push({'x':xVal, 'y':data.accz});
 
 	dps_g[0].push({'x':xVal, 'y':data.gyrx});
 	dps_g[1].push({'x':xVal, 'y':data.gyry});
@@ -147,7 +205,22 @@ function chart_add(data) {
 	}
 
 	xVal++;
-	
+}
+
+function chart_motor_add(data) {
+	dps_motor[0].push({'x':xVal, 'y':data.motor1});
+	dps_motor[1].push({'x':xVal, 'y':data.motor2});
+	dps_motor[2].push({'x':xVal, 'y':data.motor3});
+	dps_motor[3].push({'x':xVal, 'y':data.motor4});
+
+	if (dps_motor[0].length > dataLength) {
+		dps_motor[0].shift();
+		dps_motor[1].shift();
+		dps_motor[2].shift();
+		dps_motor[3].shift();	
+	}
+
+	xVal++;
 }
 
 function msg_raw_imu(data) {
@@ -164,7 +237,21 @@ function msg_raw_imu(data) {
 	$("#magz").text(data.magz);
 
 	chart_add(data);
+}
 
+function msg_motor(data) {
+	counter++;
+
+	$("#motor1").text(data.motor1); 
+	$("#motor2").text(data.motor2); 
+	$("#motor3").text(data.motor3); 
+	$("#motor4").text(data.motor4); 
+	$("#motor5").text(data.motor5); 
+	$("#motor6").text(data.motor6); 
+	$("#motor7").text(data.motor7); 
+	$("#motor8").text(data.motor8); 
+
+	chart_motor_add(data);
 }
 
 function chart_accel() {
@@ -269,6 +356,50 @@ function chart_mag() {
      });
 }
 
+function chart_motor() {
+	chart_m = new CanvasJS.Chart("chart_motor", {
+	 animationEnabled: false,
+	exportEnabled: true,
+      title:{
+        text: "Motors"              
+      },
+      legend: default_legend, 
+       axisY:{
+   			minimum: 1100,
+ 		},
+      data: [//array of dataSeries              
+        {
+         	type: "line",
+	 		markerType: "line",
+	 		dataPoints: dps_motor[0],
+			showInLegend: true,
+			name: 'BackRight'
+       },
+        {
+         	type: "line",
+	 		markerType: "line",
+	 		dataPoints: dps_motor[1],
+			showInLegend: true,
+			name: 'FrontRight'
+       },
+        {
+         	type: "line",
+	 		markerType: "line",
+	 		dataPoints: dps_motor[2],
+			showInLegend: true,
+			name: 'BackLeft'
+       },
+        {
+         	type: "line",
+	 		markerType: "line",
+	 		dataPoints: dps_motor[3],
+			showInLegend: true,
+			name: 'FrontLeft'
+       }  
+       ]
+     });
+}
+
 default_legend = {
 	cursor: "pointer",
 	itemclick: function (e) {
@@ -290,6 +421,7 @@ function websock_recv() { //we have received a message
 			switch (data.id) {
 				case 101: init_with_status(data); break;
 				case 102: msg_raw_imu(data); break;
+				case 104: msg_motor(data); break;
 			}
 		} else {
 			//console.log(data);
